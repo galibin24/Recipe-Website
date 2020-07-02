c6442501b1db8a6332c85d2c9d88047d171dadce
Nikita Galibin <galibin24@mail.ru>
2020-07-02 21:30:55 +0100

    Header quoted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import RecipeList from "./components/recipeList";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import RecipePage from "./components/RecipePage";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -14,7 +14,7 @@ function App() {
 			<Router>
 				<Switch>
 					<Container>
-						<Header />
+						{/* <Header /> */}
 						<Route path="/" exact component={RecipeList} />
 						<Route path="/:id" component={RecipePage} />
 					</Container>
